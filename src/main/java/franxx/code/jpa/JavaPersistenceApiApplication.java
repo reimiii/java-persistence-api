05ea103bbe0c0e0d28ca073fcd11782cd15ec8a9
Hilmi AM <imiia75775@proton.me>
2024-01-09 13:01:48 +0700

    Initial commit

---
+++ src/main/java/franxx/code/jpa/JavaPersistenceApiApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPersistenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPersistenceApiApplication.class, args);
+	}
+
+}
